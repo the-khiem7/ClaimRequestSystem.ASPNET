04548bb3d846d03e9d8fbb7dcc8823f6374cdd9e
Nguyễn  Nhật <nhatnhse182399@fpt.edu.vn>
2025-02-20 09:45:30 +0000

    NguyenHoangNhat - Team2 - 37 - Update claim

---
+++ ClaimRequest.Data/Data/Requests/Claim/UpdateClaimRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimRequest.DAL.Data.Requests.Claim
+{
+    public class UpdateClaimRequest
+    {
+        [Required(ErrorMessage = "Claim Id is required")]
+        public Guid ClaimId { get; set; }
+
+        [Required(ErrorMessage = "Start Date is required")]
+        public DateOnly StartDate { get; set; }
+
+        [Required(ErrorMessage = "End Date is required")]
+        public DateOnly EndDate { get; set; }
+
+        [Required(ErrorMessage = "Total Working Hours is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Total Working Hours must be a positive number")]
+        public decimal TotalWorkingHours { get; set; }
+    }
+}
