373f1f0d995a2b3f290f13b33c08e4384f5f516e
Anh Quân Nguyễn <quannase180619@fpt.edu.vn>
2025-02-20 03:33:54 +0000

    NguyenAnhQuan - 02 - 165 - DownloadClaim

--- ClaimRequest.Service/Services/Interfaces/IClaimService.cs
+++ ClaimRequest.Service/Services/Interfaces/IClaimService.cs
@@ -17,6 +17,7 @@ namespace ClaimRequest.BLL.Services.Interfaces
         Task<ViewClaimResponse> GetClaimById(Guid id);
         Task<RejectClaimResponse> RejectClaim(Guid Id, RejectClaimRequest rejectClaimRequest);
         Task<ApproveClaimResponse> ApproveClaim(Guid Id, ApproveClaimRequest approveClaimRequest);
+        Task<MemoryStream> DownloadClaimAsync(DownloadClaimRequest downloadClaimRequest);
 
     }
 }
