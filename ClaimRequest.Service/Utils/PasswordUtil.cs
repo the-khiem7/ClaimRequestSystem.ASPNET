249374ee4d21c189cfde69cb90f0d94866cfdbb2
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-02-27 01:21:52 +0700

    add hasher for Password

---
+++ ClaimRequest.Service/Utils/PasswordUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace ClaimRequest.BLL.Utils
+{
+    public static class PasswordUtil
+    {
+        private const int WorkFactor = 12;
+        public static async Task<string> HashPassword(string rawPassword)
+        {
+            return await Task.Run(() => BCrypt.Net.BCrypt.HashPassword(rawPassword, workFactor : WorkFactor));
+        }
+
+        public static async Task<bool> VerifyPassword(string rawPassword, string hashedPassword)
+        {
+            return await Task.Run(() => BCrypt.Net.BCrypt.Verify(rawPassword, hashedPassword));
+        }
+    }
+}
