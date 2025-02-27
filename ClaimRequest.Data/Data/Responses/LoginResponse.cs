4cce00d6f596ca8c24841ddd9752a2e5a7e7f3bc
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-02-28 02:29:15 +0700

    Add DTO Object for Login process

---
+++ ClaimRequest.Data/Data/Responses/LoginResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClaimRequest.DAL.Data.Entities;
+
+namespace ClaimRequest.DAL.Data.Responses
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; }
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public SystemRole Role { get; set; }
+        public Department Department { get; set; }
+
+        public LoginResponse(ClaimRequest.DAL.Data.Entities.Staff staff)
+        {
+            Id = staff.Id;
+            Email = staff.Email;
+            FullName = staff.Name;
+            Role = staff.SystemRole;
+            Department = staff.Department;
+        }
+    }
+}
