08c9d27dd41f3aad28eed6caee50cac9c12f0aaf
Hien Nguyen <hiennpqse180072@fpt.edu.vn>
2025-03-06 14:42:23 +0000

    Nguyễn Phúc Quý Hiển-02-225- Forgot password and change password

---
+++ ClaimRequest.Data/Data/Responses/Otp/ValidateOtpResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimRequest.DAL.Data.Responses.Otp
+{
+    public class ValidateOtpResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public int AttemptsLeft { get; set; }
+    }
+}
