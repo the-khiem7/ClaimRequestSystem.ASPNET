a769ef8b8068dd19d5890425bd70fb660bec98f7
HCM25_CPL_NET_04_NguyenVanDuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-03-07 19:33:46 +0000

    Nguyen Van Duy Khiem - Team2 - 73 - Login & Authentication - Change Login Response

--- ClaimRequest.API/Program.cs
+++ ClaimRequest.API/Program.cs
@@ -147,7 +147,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
