d08a4b7207a8cf3597d3bf6bd89da2cb12c7f334
Hien <hiennpqse180072@fpt.edu.vn>
2025-03-10 11:11:19 +0700

    add attempt left for Forgotpassword

--- ClaimRequest.API/Controllers/AuthController.cs
+++ ClaimRequest.API/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using ClaimRequest.BLL.Services.Interfaces;
 using ClaimRequest.DAL.Data.MetaDatas;
 using ClaimRequest.DAL.Data.Requests;
+using ClaimRequest.DAL.Data.Requests.Auth;
 using ClaimRequest.DAL.Data.Responses;
 using ClaimRequest.DAL.Data.Responses.Auth;
 using Microsoft.AspNetCore.Mvc;
@@ -22,14 +23,14 @@ namespace ClaimRequest.API.Controllers
         public async Task<IActionResult> Login([FromBody] DAL.Data.Requests.Auth.LoginRequest loginRequest)
         {
             var response = await _authService.Login(loginRequest);
-            return Ok(ApiResponseBuilder.BuildResponse(StatusCodes.Status200OK,"Login successful",response));
+            return Ok(ApiResponseBuilder.BuildResponse(StatusCodes.Status200OK, "Login successful", response));
         }
 
         [HttpPost(ApiEndPointConstant.Auth.ForgotPasswordEndpoint)]
         [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> ForgotPassword([FromBody] ClaimRequest.DAL.Data.Requests.Auth.ForgotPasswordRequest forgotPasswordRequest)
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordRequest forgotPasswordRequest)
         {
             if (!ModelState.IsValid)
             {
@@ -45,11 +46,11 @@ namespace ClaimRequest.API.Controllers
 
             var result = await _authService.ForgotPassword(forgotPasswordRequest);
 
-            if (!result)
+            if (!result.Success)
             {
                 return BadRequest(
                     ApiResponseBuilder.BuildErrorResponse<object>(
-                        null,
+                        new { AttemptsLeft = result.AttemptsLeft },
                         StatusCodes.Status400BadRequest,
                         "Failed to change password",
                         "Unable to change password with the provided information"
@@ -61,48 +62,7 @@ namespace ClaimRequest.API.Controllers
                 ApiResponseBuilder.BuildResponse<object>(
                     StatusCodes.Status200OK,
                     "Password changed successfully",
-                    null
-                )
-            );
-        }
-
-        [HttpPost(ApiEndPointConstant.Auth.ForgotPasswordEndpoint)]
-        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> ForgotPassword([FromBody] ClaimRequest.DAL.Data.Requests.Auth.ForgotPasswordRequest forgotPasswordRequest)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(
-                    ApiResponseBuilder.BuildErrorResponse<object>(
-                        null,
-                        StatusCodes.Status400BadRequest,
-                        "Invalid request",
-                        "Please provide valid email, new password, and OTP"
-                    )
-                );
-            }
-
-            var result = await _authService.ForgotPassword(forgotPasswordRequest);
-
-            if (!result)
-            {
-                return BadRequest(
-                    ApiResponseBuilder.BuildErrorResponse<object>(
-                        null,
-                        StatusCodes.Status400BadRequest,
-                        "Failed to change password",
-                        "Unable to change password with the provided information"
-                    )
-                );
-            }
-
-            return Ok(
-                ApiResponseBuilder.BuildResponse<object>(
-                    StatusCodes.Status200OK,
-                    "Password changed successfully",
-                    null
+                    new { AttemptsLeft = result.AttemptsLeft }
                 )
             );
         }
