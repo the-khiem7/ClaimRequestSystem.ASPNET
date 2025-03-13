b101f3b552e8867e4cb8fd8b040bb7abd9f3d87d
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-03-14 02:16:04 +0700

    implement logger aspect v3

--- ClaimRequest.Service/Aspects/LoggerAspect.cs
+++ ClaimRequest.Service/Aspects/LoggerAspect.cs
@@ -2,7 +2,7 @@
 using Metalama.Framework.Code;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Linq;
+using System.Reflection;
 using System.Text.Json;
 
 namespace ClaimRequest.BLL.Aspects
@@ -19,21 +19,21 @@ namespace ClaimRequest.BLL.Aspects
 
         public override void BuildAspect(IAspectBuilder<IMethod> builder)
         {
-            // Verify that the containing type has a logger field
-            builder.Advice.WithPriority(1000)
-                .UsingNamespace("Microsoft.Extensions.Logging")
-                .UsingNamespace("System.Text.Json")
-                .UsingNamespace("System.Text.Json.Serialization");
+            // Add required namespaces
+            builder.Advice.AddNamespaces(
+                "Microsoft.Extensions.Logging",
+                "System.Text.Json",
+                "System.Reflection");
         }
 
         public override dynamic? OverrideMethod()
         {
             var methodName = $"{meta.Target.Type.Name}.{meta.Target.Method.Name}";
 
-            // Get logger from the instance (_logger field)
+            // Get logger using reflection
             ILogger logger = null!;
-            var loggerField = meta.This.GetType().GetField("_logger",
-                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var type = meta.This.GetType();
+            var loggerField = type.GetField("_logger", BindingFlags.NonPublic | BindingFlags.Instance);
 
             if (loggerField != null)
             {
@@ -41,18 +41,20 @@ namespace ClaimRequest.BLL.Aspects
             }
             else
             {
-                // Try to find any field of ILogger type
-                var loggerFields = meta.This.GetType().GetFields(
-                    System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                    .FirstOrDefault(f => typeof(ILogger).IsAssignableFrom(f.FieldType));
-
-                if (loggerFields != null)
+                // Try to find any field that implements ILogger
+                var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+                foreach (var field in fields)
                 {
-                    logger = (ILogger)loggerFields.GetValue(meta.This)!;
+                    if (typeof(ILogger).IsAssignableFrom(field.FieldType))
+                    {
+                        logger = (ILogger)field.GetValue(meta.This)!;
+                        break;
+                    }
                 }
-                else
+
+                if (logger == null)
                 {
-                    throw new InvalidOperationException($"No ILogger field found in {meta.This.GetType().Name}");
+                    throw new InvalidOperationException($"No ILogger field found in {type.Name}");
                 }
             }
 
@@ -69,29 +71,34 @@ namespace ClaimRequest.BLL.Aspects
                 {
                     try
                     {
+                        // Create array to hold parameter values
                         var parameterValues = new object[parameters.Count];
-                        for (int i = 0; i < parameters.Count; i++)
-                        {
-                            parameterValues[i] = meta.Arguments[parameters[i].Name];
-                        }
 
-                        var paramInfo = JsonSerializer.Serialize(parameterValues, new JsonSerializerOptions
-                        {
-                            WriteIndented = true,
-                            MaxDepth = 3
-                        });
-                        logger.Log(entryLogLevel, "Entering {MethodName} with parameters: {Parameters}",
-                            methodName, paramInfo);
+                        // Capture parameter values using reflection since meta.Arguments is not available
+                        var args = meta.Target.Method.Parameters.Select((p, i) => new { Name = p.Name, Value = meta.Target.Method.GetParameter(i) }).ToArray();
+
+                        // Log parameter names instead of values for now
+                        var paramNames = string.Join(", ", args.Select(a => a.Name));
+
+                        // Log method entry with parameter names
+                        ((ILogger)logger).Log(
+                            entryLogLevel,
+                            "Entering {MethodName} with parameters: {Parameters}",
+                            methodName,
+                            paramNames);
                     }
                     catch (Exception ex)
                     {
-                        logger.Log(entryLogLevel, ex,
-                            "Entering {MethodName} with parameters (serialization failed)", methodName);
+                        ((ILogger)logger).Log(
+                            entryLogLevel,
+                            "Entering {MethodName} with parameters (serialization failed): {Error}",
+                            methodName,
+                            ex.Message);
                     }
                 }
                 else
                 {
-                    logger.Log(entryLogLevel, "Entering {MethodName}", methodName);
+                    ((ILogger)logger).Log(entryLogLevel, "Entering {MethodName}", methodName);
                 }
             }
 
@@ -101,29 +108,39 @@ namespace ClaimRequest.BLL.Aspects
                 var result = meta.Proceed();
 
                 // Log successful execution with return value if enabled
-                if (LogReturnValue && logger.IsEnabled(successLogLevel) && result != null)
+                if (LogReturnValue && logger.IsEnabled(successLogLevel))
                 {
-                    try
+                    if (result != null)
                     {
-                        var returnValue = JsonSerializer.Serialize(result, new JsonSerializerOptions
+                        try
                         {
-                            WriteIndented = true,
-                            MaxDepth = 2
-                        });
-                        logger.Log(successLogLevel,
-                            "Successfully executed {MethodName}, returned: {ReturnValue}",
-                            methodName, returnValue);
+                            // For complex return types, just log the type to avoid serialization issues
+                            var typeName = result.GetType().Name;
+                            ((ILogger)logger).Log(
+                                successLogLevel,
+                                "Successfully executed {MethodName}, returned object of type: {ReturnType}",
+                                methodName,
+                                typeName);
+                        }
+                        catch
+                        {
+                            ((ILogger)logger).Log(
+                                successLogLevel,
+                                "Successfully executed {MethodName}, unable to log return value",
+                                methodName);
+                        }
                     }
-                    catch (Exception ex)
+                    else
                     {
-                        logger.Log(successLogLevel, ex,
-                            "Successfully executed {MethodName}, return value (serialization failed)",
+                        ((ILogger)logger).Log(
+                            successLogLevel,
+                            "Successfully executed {MethodName}, returned null",
                             methodName);
                     }
                 }
                 else
                 {
-                    logger.Log(successLogLevel, "Successfully executed {MethodName}", methodName);
+                    ((ILogger)logger).Log(successLogLevel, "Successfully executed {MethodName}", methodName);
                 }
 
                 return result;
@@ -131,9 +148,12 @@ namespace ClaimRequest.BLL.Aspects
             catch (Exception ex)
             {
                 // Log exception and rethrow
-                logger.Log(exceptionLogLevel, ex,
+                ((ILogger)logger).Log(
+                    exceptionLogLevel,
+                    ex,
                     "Exception in {MethodName}: {ExceptionMessage}",
-                    methodName, ex.Message);
+                    methodName,
+                    ex.Message);
                 throw;
             }
         }
