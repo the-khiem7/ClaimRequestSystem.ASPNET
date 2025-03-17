ce0e67979f4a6cde6bb8a753d9b8b70cdb848f17
Nguyễn Anh Quân <syrexd42@gmail.com>
2025-03-17 16:30:19 +0700

    Update ClaimService.cs

--- ClaimRequest.Service/Services/Implements/ClaimService.cs
+++ ClaimRequest.Service/Services/Implements/ClaimService.cs
@@ -68,9 +68,6 @@ namespace ClaimRequest.BLL.Services.Implements
 
                 var claim = await _unitOfWork.GetRepository<Claim>().GetByIdAsync(claimId)
                             ?? throw new KeyNotFoundException("Claim not found.");
-                    // Get claim by ID first before starting the transaction
-                    var claim = await _unitOfWork.GetRepository<Claim>().GetByIdAsync(cancelClaimRequest.ClaimId)
-                                ?? throw new KeyNotFoundException("Claim not found.");
 
                 if (claim.Status != ClaimStatus.Draft)
                 {
