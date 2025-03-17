154e00fee9d5cd124a659637fed63a84bf807a56
Nguyễn  Nhật <nhatnhse182399@fpt.edu.vn>
2025-03-17 09:47:19 +0000

    NguyenHoangNhat - Team2 - 184 - Implement VNPay Payment API

--- ClaimRequest.Service/Services/Interfaces/IClaimService.cs
+++ ClaimRequest.Service/Services/Interfaces/IClaimService.cs
@@ -10,7 +10,7 @@ namespace ClaimRequest.BLL.Services.Interfaces
     public interface IClaimService
     {
         Task<CreateClaimResponse> CreateClaim(CreateClaimRequest createClaimRequest);
-        Task<UpdateClaimResponse> UpdateClaim(Guid Id, UpdateClaimRequest updateClaimRequest);
+        Task<UpdateClaimResponse> UpdateClaim(Guid claimId, UpdateClaimRequest updateClaimRequest);
         Task<CancelClaimResponse> CancelClaim(CancelClaimRequest cancelClaimRequest);
         Task<PagingResponse<ViewClaimResponse>> GetClaims(int pageNumber = 1, int pageSize = 20, ClaimStatus? status = null, string? viewMode = null);
 
