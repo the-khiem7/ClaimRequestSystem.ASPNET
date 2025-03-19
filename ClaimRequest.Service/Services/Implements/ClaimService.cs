4fd36d449d0d3f11f66f51422a94ec9a73ee015e
HHN257 <hoanghuunam257@gmail.com>
2025-03-19 16:15:17 +0700

    Fix Reject claim for FE

--- ClaimRequest.Service/Services/Implements/ClaimService.cs
+++ ClaimRequest.Service/Services/Implements/ClaimService.cs
@@ -297,7 +297,6 @@ namespace ClaimRequest.BLL.Services.Implements
                 claim.UpdateAt = DateTime.UtcNow;
 
                 claimRepository.UpdateAsync(claim);
-                await _unitOfWork.CommitAsync();
 
                 _logger.LogInformation("Successfully updated claim with ID {ClaimId}.", claimId);
                 return _mapper.Map<UpdateClaimResponse>(claim);
@@ -461,12 +460,6 @@ namespace ClaimRequest.BLL.Services.Implements
                         throw new UnauthorizedAccessException($"User with ID {rejectClaimRequest.ApproverId} does not have permission to reject this claim.");
                     }
                     var approverName = approver.Name ?? "Unknown Approver";
-
-                    await _unitOfWork.GetRepository<ClaimApprover>().InsertAsync(new ClaimApprover
-                    {
-                        ClaimId = pendingClaim.Id,
-                        ApproverId = rejectClaimRequest.ApproverId
-                    });
 
                     _mapper.Map(rejectClaimRequest, pendingClaim);
                     _unitOfWork.GetRepository<Claim>().UpdateAsync(pendingClaim);
