eb33c1aaf45e605fe8f19a51b02129d0396f878f
hoangngoctien04 <hoangngoctien04@gmail.com>
2025-03-22 22:44:28 +0700

    Add uploadfile

--- ClaimRequest.API/Constants/ApiEndPointConstant.cs
+++ ClaimRequest.API/Constants/ApiEndPointConstant.cs
@@ -63,7 +63,7 @@ namespace ClaimRequest.API.Constants
         {
             public const string CloudinaryEndpoint = ApiEndpoint + "/cloudinary";
             public const string UploadImage = CloudinaryEndpoint + "/upload";
-            public const string DeleteImage = CloudinaryEndpoint + "/delete/{publicId}";
+            public const string UploadFile = CloudinaryEndpoint + "/upload-file";
         }
     }
 }
