911a8fc4afe5fa437545db7ce1b79d7adcbba1c6
ThanNguyen <thannthse183895@fpt.edu.vn>
2025-03-24 13:16:53 +0700

    Create SubmitClaimServiceTest.cs

---
+++ ClaimRequest.UnitTest/Services/SubmitClaimServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using ClaimRequest.BLL.Services.Implements;
+using ClaimRequest.DAL.Data.Entities;
+using ClaimRequest.DAL.Data.Requests.Claim;
+using ClaimRequest.DAL.Data.Responses.Claim;
+using ClaimRequest.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+using ClaimEntity = ClaimRequest.DAL.Data.Entities.Claim;
+
+
+namespace ClaimRequest.UnitTest.Services
+{
+    public class SubmitClaimTests : IDisposable
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly Mock<IUnitOfWork<ClaimRequestDbContext>> _mockUnitOfWork;
+        private readonly Mock<ILogger<ClaimEntity>> _mockLogger;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<IHttpContextAccessor> _mockHttpContextAccessor;
+        private readonly Mock<IGenericRepository<ClaimEntity>> _mockClaimRepository;
+        private readonly ClaimService _claimService;
+
+        public SubmitClaimTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+            _mockUnitOfWork = new Mock<IUnitOfWork<ClaimRequestDbContext>>();
+            _mockLogger = new Mock<ILogger<ClaimEntity>>();
+            _mockMapper = new Mock<IMapper>();
+            _mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            _mockClaimRepository = new Mock<IGenericRepository<ClaimEntity>>();
+
+            _mockUnitOfWork.Setup(uow => uow.GetRepository<ClaimEntity>()).Returns(_mockClaimRepository.Object);
+            _mockUnitOfWork.Setup(uow => uow.ProcessInTransactionAsync(It.IsAny<Func<Task<CancelClaimResponse>>>()))
+                .Returns<Func<Task<CancelClaimResponse>>>(operation => operation());
+
+            _claimService = new ClaimService(
+                _mockUnitOfWork.Object,
+                _mockLogger.Object,
+                _mockMapper.Object,
+                _mockHttpContextAccessor.Object
+            );
+        }
+
+        public void Dispose() => GC.SuppressFinalize(this);
+    }
+}
