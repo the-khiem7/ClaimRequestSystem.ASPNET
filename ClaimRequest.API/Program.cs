f1389818f0e2d291a9c7d914c01aea3f9b00a918
DoDat04 <dothanhdat339@gmail.com>
2025-04-01 00:24:57 +0700

    add pendingReminder api endpoint

--- ClaimRequest.API/Program.cs
+++ ClaimRequest.API/Program.cs
@@ -93,6 +93,7 @@ void RegisterApplicationServices()
     builder.Services.AddScoped<IGenericRepository<Payment>, GenericRepository<Payment>>();
     builder.Services.AddScoped<IRefreshTokensService, RefreshTokensService>();
     builder.Services.AddScoped<IPasswordReminderService, PasswordReminderService>();
+    builder.Services.AddScoped<IPendingReminderService, PendingReminderService>();
 }
 
 // Method to configure the database
